309385db25a7d05b9f53a8485f73868787c4986a
ajalaj <aditi.jalaj@gmail.com>
2021-06-16 19:44:56 +0530

    1st version

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -7,8 +7,6 @@ const VideoDetails = () => {
     const det = JSON.parse(localStorage.getItem(id));
     const url = (det[0].video_url);
     
-    console.log('type of ',typeof(url) , 'and url',url)
-    
     //split into two and get the part after =
     var res=url.split("=")
     var embedUrl="https://www.youtube.com/embed/"+res[1];
